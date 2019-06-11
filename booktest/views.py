240d5862f85f9d8ea4d50164be68bcf993a1114b
oldestcrab <oledestcrab@gmail.com>
2019-06-11 16:58:27 +0800

    start

--- booktest/views.py
+++ booktest/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(reqeust):
+    return HttpResponse('hello')
+
+def detail(reqeust, p1, p2, p3):
+    return HttpResponse('year:{p1},month:{p2},day:{p3}'.format(p1=p1, p2=p2, p3=p3))
+
+def get_test1(request):
+    return render(request, 'booktest/get_test1.html')
+
+def get_test2(request):
+    return render(request, 'booktest/get_test2.html')
+
+def get_test3(request):
+    return render(request, 'booktest/get_test3.html')
