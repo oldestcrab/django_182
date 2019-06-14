4b34ae8b9815fdd4d5ec8bc3e2ad2c5851a11861
oldestcrab <oledestcrab@gmail.com>
2019-06-14 16:56:45 +0800

    学习中

--- booktest/urls.py
+++ booktest/urls.py
@@ -3,4 +3,15 @@ from . import views
 urlpatterns = [
     url(r'^index$', views.index),
     url(r'^(?P<p2>\d+)/(?P<p3>\d+)/(?P<p1>\d+)$', views.detail),
+    url(r'^get_test1/$', views.get_test1),
+    url(r'^get_test2/$', views.get_test2),
+    url(r'^get_test3/$', views.get_test3),
+
+    url(r'^post_test1/$', views.post_test1),
+    url(r'^post_test2/$', views.post_test2),
+
+    url(r'^session1/$', views.session1),
+    url(r'^session2/$', views.session2),
+    url(r'^session3/$', views.session3),
+    url(r'^session2_handle/$', views.session2_handle),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
@@ -12,7 +12,46 @@ def get_test1(request):
     return render(request, 'booktest/get_test1.html')
 
 def get_test2(request):
-    return render(request, 'booktest/get_test2.html')
+    a1 = request.GET['a']
+    b1 = request.GET['b']
+    c1 = request.GET['c']
+    context = {'a':a1, 'b':b1, 'c':c1}
+    return render(request, 'booktest/get_test2.html', context)
 
 def get_test3(request):
-    return render(request, 'booktest/get_test3.html')
+    a1 = request.GET.getlist('a')
+    context = {'a':a1}
+    return render(request, 'booktest/get_test3.html', context)
+
+def post_test1(request):
+
+    return render(request, 'booktest/post_test1.html')
+
+def post_test2(request):
+    uname = request.POST['uname']
+    upwd = request.POST['upwd']
+    ugender = request.POST['ugender']
+    uhobby = request.POST.getlist('uhobby')
+    context = {'uname':uname, 'upwd':upwd, 'ugender':ugender, 'uhobby':uhobby}
+    return render(request, 'booktest/post_test2.html', context)
+
+def session1(request):
+    uname = request.session.get('myname')
+    context = {'uname':uname}
+
+    return render(request, 'booktest/session1.html', context)
+
+def session2(request):
+
+
+    return render(request, 'booktest/session2.html')
+
+def session2_handle(request):
+
+    uname = request.POST['uname']
+    request.session['myname'] = uname
+    return redirect('/booktest/session1/')
+
+def session3(request):
+    del request.session['myname']
+    return redirect('/booktest/session1/')
